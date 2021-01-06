5a95ebf4b3e7ef03aa46e671605da507cdc5be32
Driesheylen1 <heylendries4@gmail.com>
2021-01-06 15:43:55 +0100

    dif. exprression compared between invasiveness big cluster groups

--- differential_expression.R
+++ differential_expression.R
@@ -11,19 +11,19 @@ library(writexl)
 
 
 
-iam = randomizing2
+iam = Sepsis_data_invasivity_normalized_outliers_out
 #not needed this
 #iam = melt(iam, id.vars = c("ID", "Batch", "Sex", "Age", "Diagnosis", "In_Hospital_Mortality",
                             "ICU_admission", "severity_groups", "Diag_cat_number", "CCI", "Gram_stain", "SOFA_score", "WBC", "CRP",
           # variable.name = "assay", value.name = "npx")
 
 
-tt = table(iam$severity_groups)
+tt = table(iam$Invasiveness_groups)
 
 
-
-sepsis_unfavourable <- subset(iam, severity_groups %in% names(tt[tt = 1])) #to get only unfavourable outcome
-sepsis_favourable <- subset(iam, severity_groups %in% names(tt[tt = 2])) #to get only favourable outcome
+#2 is not invasive and 1 is invasive (replaced 0 by 2)
+sepsis_unfavourable <- subset(iam, Invasiveness_groups %in% names(tt[tt = 1])) #to get only unfavourable outcome
+sepsis_favourable <- subset(iam, Invasiveness_groups %in% names(tt[tt = 2])) #to get only favourable outcome (not invasive)
 #t.test(sepsis_favourable$`CSF-1`, sepsis_unfavourable$IL8, var.equal = FALSE)
        
 #cleaning data       
@@ -35,5 +35,5 @@ protein_expression = unlist(protein_expression)
 protein_expression = as.data.frame(protein_expression)
 rownames = rownames(protein_expression)
 protein_expression = cbind(rownames, protein_expression)
-write_xlsx(x = protein_expression, "C:/Users/HEYLEND/Desktop/introductiondecember/differential_expressionrandom_severity_size2.xlsx",
+write_xlsx(x = protein_expression, "C:/Users/HEYLEND/Desktop/introductiondecember/differential_expression_invasivity.xlsx",
             use_zip64 = T)
