5dd1c8aa45bec72d74ddb325d1cacbdb36a5650c
Driesheylen1 <heylendries4@gmail.com>
2021-01-15 14:19:10 +0100

    annotated

--- anova_sepsis.R
+++ anova_sepsis.R
@@ -1,13 +1,14 @@
 library(reshape2)
-path <- ("Y:\\Unit_HEALTH/RD_SPEC/Platform Systems Biology/3 PhD & PostDoc/PhD_Xomics/02_Data analyses/R codes")
+path <- ("Y:\\Unit_HEALTH/RD_SPEC/Platform Systems Biology/3 PhD & PostDoc/PhD_Xomics/02_Data analyses/R codes/Rdump/")
 setwd(paste0(path))
-Sepsis_anov_grouping <- read.csv("Copy of Sepsis_anov_grouping.csv", header = T, sep = ";")
-Y:\\Unit_HEALTH/RD_SPEC/Platform Systems Biology/3 PhD & PostDoc/PhD_Xomics/02_Data analyses/R codes/Rdump/
+#Sepsis_anov_grouping <- read.csv("Copy of Sepsis_anov_grouping.csv", header = T, sep = ";")
+
 iam = melt(iam, id.vars = c("ID", "Anova_groups"),
           variable.name = "assay", value.name = "npx")
 x = unique(iam$assay)
 head(x)
 
+# x will be inserted in the my function by using subset apply function
 models = sapply(x, function(my) {
   lm(npx ~ Anova_groups, data = iam, assay==my)
 }, simplify = FALSE)
@@ -18,14 +19,12 @@ ANOVA.tables <- sapply(models, aov, simplify=FALSE)
 summary(ANOVA.tables[[1]])
 #multiple comparison for assay 1
 TukeyHSD(ANOVA.tables[[1]])
-anova_outcome <- 0
-print("This loop calculates the square of the first 92 elements of vector u1")
+anova_outcome <- 0 #creating an empty list in which outcome of loop can be stored
+print("This loop calculates the square of the first 92 elements of vector ANOVA.tables")
 
-# Initialize `usq`
-
+#to generate the entire anova list for each protein at once
 for(i in 1:92) {
-  # i-th element of `u1` squared into `i`-th position of `usq`
-  anova_outcome[i] <- TukeyHSD(ANOVA.tables[[i]])
+  anova_outcome[i] <- TukeyHSD(ANOVA.tables[[i]]) #mark the double brackets = crucial
   print(anova_outcome[i])
 }
 
